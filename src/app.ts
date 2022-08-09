651947c72ac75a5b0a67d928403aed3ec752aec1
cassandra-mn <arthenciac@gmail.com>
2022-08-09 19:02:32 -0300

    test

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ app.use(json());
 
 app.get("/students", async (req: Request, res: Response) => {
   const students = await prisma.student.findMany();
+  console.log('estou testando');
   res.send(students);
 });
 
